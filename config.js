6082d4c48a7afe4e2dceb6fca031256ce2e2358d
phamhieu2k <92872717+phamhieu2k@users.noreply.github.com>
2021-10-20 22:15:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc em 20/10 tràn ngập niềm vui và những nụ cư
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = '1.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
